10b09f1d3df9c7985a1a0892de8cfbcad2b00e77
Stan Boer <stanboer@gmail.com>
2021-09-22 00:29:48 +0200

    Initiated c++ project and added ASCII colour scheme

---
+++ RayTracingASCII/main.cpp
@@ -0,0 +1,44 @@
+// Includes
+#include "settings.h"
+
+/*
+*							ASCII Grayscale
+*  <-- Denser ----------------------------------------------- Sparser --> 
+* "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. "
+*/
+const char palette[] = ".'`,^:\";~LCJUYXZO0Q";
+
+int main()
+{
+	std::cout << "Initiating screen.\n";
+
+	for (auto colour : palette) {
+		std::cout << colour << "\n";
+	}
+
+	for (int paletteIndex = 0; paletteIndex < strlen(palette); paletteIndex++) {
+		if (paletteIndex % 2 != 0) {
+			std::cout << paletteIndex << ": " << palette[paletteIndex] << "\n";
+		}
+	}
+
+	//grid(HEIGHT, WIDTH);
+
+	return 0;
+}
+
+// Prints out amount of rows and columns of given window size as coordinates
+void grid(int x, int y)
+{
+	int rows, columns;
+
+	for (rows = 0; rows < x; rows++)
+	{
+		for (columns = 0; columns < y; columns++)
+		{
+			// Add 1 to rows and columns for offset as they start at 1 and not 0 (for users)
+			std::cout << "(" << (rows + 1) << ", " << (columns + 1) << "), ";
+		}
+		std::cout << " \n";
+	}
+}
---
+++ RayTracingASCII/settings.h
@@ -0,0 +1,13 @@
+﻿// RayTracingASCII.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+#define HEIGHT	800
+#define WIDTH	600
+
+// TODO: Reference additional headers your program requires here.
+
+void grid(int HEIGHT, int WIDTH);
